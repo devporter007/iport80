a16fd510e26dcf9ab2dd580e768d62511b69a798
devporter007 <portzero7@protonmail.com>
2024-09-30 02:18:38 +0530

    implemented memory and now 8bit regs.

--- main.cpp
+++ main.cpp
@@ -1,43 +1,56 @@
 #include <bits/stdc++.h> // not recommended but for ease im using it for now.
 using namespace std;
 // aims to replicate arch design of 8085 with some changes
-// memory specifications: 64kx16 RAM;
-// 16 bit registers.
+// memory specifications: 64k RAM+ROM(8bit);
+// 8 bit registers.
 // This design will follow the modular approach where each component is complete in itself.
 // converts hex to int;
 // Custom instructions are succeded with XP tag, eg: DIVXP, MULXP;
 // 8 bit opcode as standard in 8085
+
+class mem {
+public:
+    array<bitset<8>, 65536> memory;
+    bitset<16> rom_end_address;
+    explicit mem(uint16_t rom_size = 16384) {
+        rom_end_address = bitset<16>(rom_size - 1);
+    }
+    void write(bitset<16> address, bitset<8> value) {
+        if (address.to_ulong() > rom_end_address.to_ulong()) {
+            memory[address.to_ulong()] = value;
+        }
+    }
+    bitset<8> read(bitset<16> address) {
+        return memory[address.to_ulong()];
+    }
+};
+
 class eightfive{
-    // Registers, each to use 16 bits;
-    public:
-        bitset<16> A,B,C,D,H,L;
     private:
-        bitset<16> W,Z;
+        bitset<8> W,Z;
+        mem memory;
     public:
-        bitset<8> flags; // S(7) Z(6) x(5) AC(4) x(3) P(2) x(1) Cy(0)
-        stack<bitset<16>> mainstack;
-        bitset<16> programCounter;
+        bitset<8> A,B,C,D,H,L;
         eightfive(){
             A = B = C = D = H = L = 0;
             programCounter = 0;
-        } // constructor
-        // Addition:
-        // ADD/ADI (0)
-        // ADC/ACI (1)
-        // Substraction:
-        // SUB/SUI (2)
+        }
+        bitset<8> flags; // S(7) Z(6) x(5) AC(4) x(3) P(2) x(1) Cy(0)
+        stack<bitset<16>> mainstack;
+        bitset<16> programCounter;
+
         void parity(){
-            bitset<16> store(0b0000000000000000);
-            for (int j=0; j<16; j++){
+            bitset<8> store(0b00000000);
+            for (int j=0; j<8; j++){
                 store[0] = store[0]^A[j];
             }
-            if (store == 0b0000000000000001) flags[2] = false;
+            if (store == 0b00000001) flags[2] = false;
             else flags[2] = true;
         }
-        void ALU(bitset<16> input, bitset<8> controller){// ALU ONLY TAKES VALUES AS INPUTS, ONLY VALUES.
+        void ALU(bitset<8> input, bitset<8> controller){// ALU ONLY TAKES VALUES AS INPUTS, ONLY VALUES.
             if (controller == 0b00000000 || controller == 0b00000001){ //0 or 1
                 bitset<1> cin = (controller == 0b00000001) ? flags[0] : 0;
-                for (int i = 0; i <= 15; i++) {
+                for (int i = 0; i <= 7; i++) {
                     if (A[i] == 0b0 && input[i] == 0b0 && (flags[0] == 0b0)) {
                         A[i] = 0b0;
                         if (cin == 1) A[i] = 0b1;
@@ -54,23 +67,23 @@ class eightfive{
                     else if ((A[i] == 0b0 && input[i] == 0b1 || A[i] == 0b1 && input[i] == 0b0) && (flags[0] == 0b1)) {
                         A[i] = 0b0;
                         flags[0] = 0b1;
-                        if (i == 7) flags[4] = 0b1; // Auxiliary Carry
+                        if (i == 3) flags[4] = 0b1; // Auxiliary Carry
                     }
                     else if (A[i] == 0b1 && input[i] == 0b1 && flags[0] == 0b0) {
                         A[i] = 0b0;
                         flags[0] = 0b1;
-                        if (i == 7) flags[4] = 0b1; // Auxiliary Carry
+                        if (i == 3) flags[4] = 0b1; // Auxiliary Carry
                     }
                     else if (A[i] == 0b1 && input[i] == 0b1 && flags[0] == 0b1) {
                         A[i] = 0b1;
                         flags[0] = 0b1;
-                        if (i == 7) flags[4] = 0b1;
+                        if (i == 3) flags[4] = 0b1;
                     }
-                    if (i == 15 && A[15] == 0b1) flags[7] = 0b1;
-                    else if (i == 15 && A[15] == 0b0) flags[7] = 0b0;
+                    if (i == 7 && A[7] == 0b1) flags[7] = 0b1;
+                    else if (i == 7 && A[7] == 0b0) flags[7] = 0b0;
                 }
                 // Check for zero flag
-                if (A == 0b0000000000000000) flags[6] = 0b1;
+                if (A == 0b00000000) flags[6] = 0b1;
                 else flags[6] = 0b0;
                 // Update parity flag
                 parity();
@@ -79,13 +92,13 @@ class eightfive{
             else if (controller == 0b00000011){//3 SUB/SUI
                 // to substract stuff which is A = A - INPUT here, we take the 2s complement of INPUT and then perform ADD.
                 input.flip();
-                input = bitset<16>(input.to_ulong() + 1);
+                input = bitset<8>(input.to_ulong() + 1);
                 ALU(input,0b00000000);
             }
         };
 };
 void printResult(eightfive &cpu) {
-    cout << cpu.A <<endl;
+    cout << "Result " << cpu.A <<endl;
     for (int i = 7; i >= 0; i--) {
         cout << cpu.flags[i] << " ";
     }
@@ -99,8 +112,8 @@ void resetCPU(eightfive cpu){
 int main() {
     eightfive cpu;
     resetCPU(cpu);
-    cpu.A = 0b1111111111111111;
-    cpu.ALU(0b1111111111111111,0b00000011);
+    cpu.A = 0b11111111;
+    cpu.ALU(0b11111111,0b00000011);
     printResult(cpu);
     return 0;
 }
